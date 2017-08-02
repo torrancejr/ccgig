0d7691b404f31bbe3fd778260291fc2241abe05f
torrancejr <torrance.jr@gmail.com>
2017-08-01 21:52:53 -0400

    react user reviews

--- react/src/main.js
+++ react/src/main.js
@@ -8,11 +8,7 @@ $(function() {
   let app = document.getElementById( 'app' );
   if (app) {
   ReactDOM.render(
-<<<<<<< HEAD
-      <h1>User Reviews Coming Soon</h1>,
-=======
       <UsersList />,
->>>>>>> react
       app
     );
   };
