4618d6deb0e512a4d67eebee5150bacd7682a17e
torrancejr <torrance.jr@gmail.com>
2017-08-03 15:02:19 -0400

    feature stars

--- spec/tasks/user_signs_up_spec.rb
+++ spec/tasks/user_signs_up_spec.rb
@@ -2,19 +2,17 @@ require 'rails_helper'
 
 feature "User signs up" do
   scenario 'a new user signs up' do
-    visit root_path
+
+    visit new_user_registration_path
+    fill_in 'First Name', with: 'Ryan'
+    fill_in 'Last Name', with: 'Torrance'
+    fill_in 'Phone', with: '6106565293'
+    fill_in 'Username', with: 'bobbytables'
+    fill_in 'Email', with: 'bobbytables@gmail.com'
+    fill_in 'Password', with: 'testtest'
+    fill_in 'Password Confirmation', with: 'testtest'
 
     click_link 'Sign Up'
 
-    fill_in 'First Name', with: "Ryan"
-    fill_in 'Last Name', with: "Torrance"
-    fill_in 'Email', with: "bobbytables@gmail.com"
-    fill_in 'Password', with: "testtest"
-    fill_in 'Password Confirmation', with: "testtest"
-
-    click_button 'Sign Up'
-
-    expect(page).to have_content('Welcome! You have signed up successfully.')
-    expect(page).to have_content('Sign Out')
   end
 end
